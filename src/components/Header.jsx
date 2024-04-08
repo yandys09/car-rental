04cd6d5ffc48a2553423fecf0bc115681c891c28
yandys09 <yandys03@naver.com>
2024-04-08 20:10:43 +0900

    filed add

---
+++ src/components/Header.jsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { links } from "@/contants/appdata";
+import Link from "next/link";
+import React, { useState } from "react";
+import { usePathname } from "next/navigation";
+
+const Header = ({ params }) => {
+  const pathname = usePathname();
+  const [isMenu, setIsMenu] = useState(false);
+
+  console.log(pathname);
+  return (
+    <div className="bg-white shadow-lg h-16 flex justify-between items-center md:px-[5%]">
+      <Link
+        href="/"
+        className="text-third-color font-bold text-2xl pl-5 md:pl-0"
+      >
+        Code Scrapper
+      </Link>
+      <div
+        className="md:hidden flex flex-col gap-1 pr-5 "
+        onClick={() => setIsMenu(!isMenu)}
+      >
+        <div className="w-5 h-0.5 bg-black"></div>
+        <div className="w-5 h-0.5 bg-black"></div>
+        <div className="w-5 h-0.5 bg-black"></div>
+      </div>
+      <div
+        className={` ${
+          isMenu ? "flex flex-col absolute top-16 w-full" : "hidden md:flex"
+        } gap-5 px-5 md:flex-row md:static bg-white md:w-auto text-center `}
+      >
+        {links?.map((link) => (
+          <div key={link.link}>
+            <Link
+              className={`${
+                pathname === link.link ? "text-third-color" : "text-gray-400"
+              }`}
+              href={link.link}
+              exact={link.exact}
+            >
+              <span>{link.text}</span>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
